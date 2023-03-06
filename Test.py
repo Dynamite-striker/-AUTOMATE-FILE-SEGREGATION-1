46dd11f1fec2e0e52468c7c3062250e1fbd996d6
Dynamite-striker <111241350+Dynamite-striker@users.noreply.github.com>
2023-03-06 19:14:08 +0000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+images = [f for f in os.listdir() if '.jpg' in f.lower()]
+
+os.mkdir('downloaded_images')
+
+for image in images:
+    new_path = 'downloaded_images/' + image
+    shutil.move(image, new_path)
